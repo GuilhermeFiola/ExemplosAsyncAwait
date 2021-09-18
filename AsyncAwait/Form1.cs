1d84a9fe2a7dc02c1a4501c172f00bf36fcc4a79
Guilherme Fiola <guilherme.fiola@pixeon.com>
2021-09-18 18:24:02 -0300

    Ajustes do projeto e do arquivo MD

---
+++ AsyncAwait/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwait
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var retorno = await RetornaRequestAsync();
+            //var retorno = RetornaRequest();
+            richTextBox1.Text = retorno;
+        }
+
+        private async Task<string> RetornaRequestAsync()
+        {
+            var httpClient = new HttpClient();
+
+            var tarefa = httpClient.GetAsync("http://google.com.br");
+
+            var dataHoraConsulta = DateTime.Now;
+
+            await Task.Delay(5000);
+
+            var request = await tarefa;
+
+            return string.Join(" - ", request, dataHoraConsulta);
+        }
+
+        private string RetornaRequest()
+        {
+            var httpClient = new HttpClient();
+
+            var request = httpClient.GetAsync("http://google.com.br").Result;
+
+            Thread.Sleep(5000);
+
+            var dataHoraConsulta = DateTime.Now;
+
+            return string.Join(" - ", request, dataHoraConsulta);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Projeto de exemplo para a apresentação de boas práticas com Async/Await
+
+O objetivo desse repositório é servir como consulta com exemplos da utilização de métodos assíncronos do mundo .NET
+
+A apresentação pode ser encontrada em: [Boas práticas com Async/Await](https://docs.google.com/presentation/d/1Nwo0hzxdzoGD6i_lAcY62iXimFiui6Ag9L6wPT8frYM/edit?usp=sharing)
